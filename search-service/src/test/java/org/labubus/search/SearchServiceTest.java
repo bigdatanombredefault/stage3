3c1181b614562b58a1c9cf9b77d1ca22e748316f
albertuti1910 <alberto.rivero106@alu.ulpgc.es>
2026-01-15 00:43:35 +0000

    feat: enhance SearchServiceTest with additional test cases for search functionality and year filtering

--- search-service/src/test/java/org/labubus/search/SearchServiceTest.java
+++ search-service/src/test/java/org/labubus/search/SearchServiceTest.java
@@ -1,4 +1,82 @@
 package org.labubus.search;
 
-public class SearchServiceTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.labubus.model.BookMetadata;
+import org.labubus.search.model.SearchResult;
+import org.labubus.search.service.SearchService;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.multimap.MultiMap;
+
+class SearchServiceTest {
+
+	private HazelcastInstance hazelcast;
+
+	@AfterEach
+	@SuppressWarnings("unused")
+	public void tearDown() {
+		if (hazelcast != null) {
+			hazelcast.shutdown();
+			hazelcast = null;
+		}
+	}
+
+	@Test
+	void searchService_search_returnsRankedResults() {
+		Config config = new Config();
+		config.setClusterName("test-" + UUID.randomUUID());
+		hazelcast = Hazelcast.newHazelcastInstance(config);
+
+		String metadataMapName = "metadata";
+		String invertedIndexName = "inverted";
+
+		BookMetadata book1 = new BookMetadata(1, "Magic Mountain", "Thomas Mann", "en", 1924, "/tmp/1");
+		BookMetadata book2 = new BookMetadata(2, "Plain Book", "Anon", "en", 2000, "/tmp/2");
+		hazelcast.getMap(metadataMapName).put(1, book1);
+		hazelcast.getMap(metadataMapName).put(2, book2);
+
+		MultiMap<String, Integer> index = hazelcast.getMultiMap(invertedIndexName);
+		index.put("magic", 1);
+		index.put("mountain", 1);
+		index.put("plain", 2);
+
+		SearchService service = new SearchService(hazelcast, 10, metadataMapName, invertedIndexName);
+		List<SearchResult> results = service.search("magic", null, null, null, 10);
+		assertEquals(1, results.size());
+		assertEquals(1, results.get(0).bookId());
+		assertTrue(results.get(0).score() > 0);
+	}
+
+	@Test
+	void searchService_filtersByYear() {
+		Config config = new Config();
+		config.setClusterName("test-" + UUID.randomUUID());
+		hazelcast = Hazelcast.newHazelcastInstance(config);
+
+		String metadataMapName = "metadata";
+		String invertedIndexName = "inverted";
+
+		BookMetadata book1 = new BookMetadata(1, "Test", "A", "en", 1999, "/tmp/1");
+		BookMetadata book2 = new BookMetadata(2, "Test", "B", "en", 2000, "/tmp/2");
+		hazelcast.getMap(metadataMapName).put(1, book1);
+		hazelcast.getMap(metadataMapName).put(2, book2);
+
+		MultiMap<String, Integer> index = hazelcast.getMultiMap(invertedIndexName);
+		index.put("test", 1);
+		index.put("test", 2);
+
+		SearchService service = new SearchService(hazelcast, 10, metadataMapName, invertedIndexName);
+		List<SearchResult> results = service.search("test", null, null, 2000, 10);
+		assertEquals(1, results.size());
+		assertEquals(2, results.get(0).bookId());
+	}
 }
